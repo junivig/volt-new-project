53dfe26c3361e30f4e8482c7d721ba1c2e13910b
junivig <junaid@vigoroustechnologies.com>
2025-02-28 19:45:05 +0500

    api's and changing done

---
+++ .history/assets/js/constant_20250228130857.js
@@ -0,0 +1,3 @@
+// assets/js/constant.js
+const apiBaseUrl = 'https://192.168.131.152/api/';
+const imgBaseUrl = 'https://192.168.131.152/uploads/';
---
+++ .history/assets/js/constant_20250228131441.js
@@ -0,0 +1,3 @@
+// assets/js/constant.js
+const apiBaseUrl = 'http://192.168.131.152:8080/api/';
+const imgBaseUrl = 'http://192.168.131.152:8080/uploads/';
